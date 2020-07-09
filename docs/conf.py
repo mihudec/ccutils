077df3f35d1b571f11c4350446aa99230a23c8f7
mijujda <mijujda@gmail.com>
2020-07-09 16:36:47 +0200

    Version 0.2.10
    Bump version number

--- docs/conf.py
+++ docs/conf.py
@@ -26,9 +26,9 @@ author = 'mihudec'
 add_module_names = False
 
 # The short X.Y version
-version = '0.2.9'
+version = '0.2.10'
 # The full version, including alpha/beta/rc tags
-release = '0.2.9-beta'
+release = '0.2.10-beta'
 
 
 # -- General configuration ---------------------------------------------------
